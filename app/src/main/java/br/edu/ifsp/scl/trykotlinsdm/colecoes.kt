4d26681f5e80262dec7f286eeb3b015c19041cc0
gustavolopes899 <gustavolopes899@gmail.com>
2019-09-06 20:48:36 -0300

    Exemplo com IntRange

--- app/src/main/java/br/edu/ifsp/scl/trykotlinsdm/colecoes.kt
+++ app/src/main/java/br/edu/ifsp/scl/trykotlinsdm/colecoes.kt
@@ -11,4 +11,16 @@ fun main(args: Array<String>) {
     for (integrante in familia) {
         println(integrante)
     }
+
+    // Lista de inteiros
+    val listanInteiros: List<Int> = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    // Percorrer com for usando IntRange
+    for (i in 0..9) {
+        println(listanInteiros[i])
+    }
+    // listaInteiros.add(11) -> Erro - Lista imutável
+    // listaInteiros = listof() -> Erro - referÊncia é imutável
+
+    //
 }
